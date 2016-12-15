e6a60231641dd4b644ecc892d7ca4e3ed4e5d254
yisheng90 <yisheng@yishenglee.com>
2016-12-15 15:26:43 +0800

    added solution

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,19 @@
 // your code here:
+var time = 0
+var timer
+$('#start').click(function () {
+  timer = setInterval(function () {
+    time += 1
+    $('#timer').text('Time elapsed: ' + time)
+  }, 1000)
+})
+
+$('#pause').click(function () {
+  clearInterval(timer)
+})
+
+$('#reset').click(function () {
+  clearInterval(timer)
+  time = 0
+  $('#timer').text('Stop Watch')
+})
